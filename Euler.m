ae936c489925d5bf02aedf161de0c92385241c8d
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-02-22 19:44:47 -0500

    Update Euler.m

--- Euler.m
+++ Euler.m
@@ -1,6 +1,6 @@
 function [t, w] = Euler(f, to, yo, h, n)
   
-  #Euler's method initial value problem.
+  # Euler's method initial value problem.
   
 w=[yo];
 t=[to];
@@ -9,3 +9,19 @@ w=[w ; w(i)+h*f(t(i),w(i))];
 t=[t ; t(i)+h];
 endfor
 endfunction
+
+  # Example:
+  # [t,w] = Euler(@(t,y)t*y+t^3,0,1,1/3,3)
+  
+  # WE GET:
+  
+  # t =
+    # 0.00000
+    # 0.33333
+    # 0.66667
+    # 1.00000
+  # w =
+    # 1.0000
+    # 1.0000
+    # 1.1235
+    # 1.4719
